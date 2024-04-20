78234ed965b6eada82417d1838ae9419137edec5
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-04-20 12:03:32 -0300

    First commit - Criação de um projeto de gerenciamento de estoque.

---
+++ src/test/java/com/github/marciokleber/storage/StorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
